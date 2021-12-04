34ac61d06aa7d5f04bf501264715890c6862f7a5
Laxr24 <73450829+Laxr24@users.noreply.github.com>
2021-12-04 12:14:02 +0600

    Bug fix - Feature update
    Fixed issues:
    
    *Content update
    * Slow server resposen
    
    Added features:
    * Update content
    * Update confirmation alert
    * Reset Content
    * Restructured style
    * Restructured UI on desktop
    * Emoji containing aletrs and confirmation

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -15,27 +15,32 @@
 
 
 </head>
-<body class="bg-gray-900 p-3" >
+<body class="bg-gray-900 md:px-20 px-3 py-3" >
     
     
-    <p class="text-white font-mono text-center md:text-left">Here are he file lists:</p>
-
-  
-    <ul>
-        @foreach ($files as $key=>$value)
-            <li>
-                <span class="mr-3 text-red-400">{{$key}}.</span> 
-                <a class="text-white inline-block ml-4 hover:text-pink-400 transition"  href="#" onclick="fetchFile(this)">{{$value}}</a> 
-                <a href="#" class="text-red-500 transition hover:text-gray-300 uppercase ml-2">delete</a>
-            </li>    
-        @endforeach
-    </ul>
-
-    <div class="container  mx-auto pt-4">
-        <p class="font-mono uppercase text-pink-300 text-center p-2">Showing content of: <span class="text-green-200" id="showFileName"></span></p>
-        <textarea id="fileContent" class="p-2 w-full  font-mono tracking-wider border-none outline-none rounded-md text-white bg-gray-700" rows="20"></textarea>
-
-        <button class="rounded-md bg-gradient-to-br from-green-400 to-green-300 px-4 py-2 m-2" onclick="updateContent()">Update</button>
+    <div class="grid md:grid-cols-2 grid-cols-1 gap-2">
+        <div class="container  justify-center items-center flex">
+            <div>
+                <p class=" text-lg text-green-400 font-mono text-center md:text-left">Here are the model files 🏀</p>
+            <ul>
+                @foreach ($files as $key=>$value)
+                    <li class="my-2">
+                        <span class="mr-3 text-red-400">{{$key}}.</span> 
+                        <a class="text-white inline-block ml-4 hover:text-pink-400 transition"  href="#" onclick="fetchFile(this)">{{$value}}</a> 
+                        <a href="#" class="text-red-500 transition hover:text-gray-300 uppercase ml-2">delete</a>
+                    </li>    
+                @endforeach
+            </ul>
+            </div>
+        </div>
+    
+        <div class="container pt-4 ">
+            <p class="font-mono uppercase text-pink-300 text-center p-2">Showing content of: <span class="text-green-200" id="showFileName"></span></p>
+            <textarea id="fileContent" class="p-2 w-full  font-mono tracking-wider border-none outline-none rounded-md text-white bg-gray-700" rows="20"></textarea>
+    
+            <button class="rounded-md bg-gradient-to-br from-green-400 to-green-300 px-4 py-2 m-2" onclick="updateContent()">Update</button>
+            <button class="rounded-md bg-gradient-to-br from-yellow-400 to-yellow-300 px-4 py-2 m-2" onclick="resetFile()">Reset</button>
+        </div>
     </div>
 
 
@@ -64,10 +69,22 @@
             axios.post(window.location.origin+'/api/content/'+filename, {data: updatedData}).then(res=>{
                 if(res.data.status == 200){
                     content.value = res.data.content
-                    alert('Content updated successfully!')
+                    alert('Content updated successfully! ✅')
                 }
                 console.log(res.data.content); 
             })
+        }
+
+
+        function resetFile(){
+            if(content.value != '' && showFileName != ''){
+                var a = confirm('Empty document? 🗑️'); 
+                if(a){
+                    content.value=''} 
+            }
+            else{
+                alert('No file selected to reset! ⛔ \nSelect a file and try ♻️')
+            }
         }
         
     </script>
